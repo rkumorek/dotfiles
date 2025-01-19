10fc66a6b7d3754f8477af1b4e997bd0eedb5b59
rkumorek <rafal.kumorek@gmail.com>
2025-01-19 21:14:13 +0000

    Update nvim

---
+++ config/nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+	-- {
+	--  'stevearc/oil.nvim',
+	-- ---@module 'oil'
+	-- ---@type oil.SetupOpts
+	--opts = {},
+	-- Optional dependencies
+	--ependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	-- }
+}
---
+++ config/nvim/lua/config/plugins/sleuth.lua
@@ -0,0 +1,8 @@
+-- Detect tabstop and shiftwidth automatically
+
+return {
+	{
+		"tpope/vim-sleuth",
+		tag = "v2.0",
+	},
+}
